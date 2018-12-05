77a3c86478d6cd75a68870de1d1fc0be44b085ec
hmh49 <hmh49@pitt.edu>
2018-12-05 13:37:47 -0500

    updated webpage

---
+++ javaintro.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded',init,false);
+
+function init() {
+    alert ('The page loaded!!!');
+    var buttons = document.getElementsByTagName("button")
+buttons[0].addEventListener('click', changeColor,false)
+buttons[1].addEventListener('click', changeColor2, false)
+buttons[2].addEventListener('click', newFunction, false)
+}
+
+
+function changeColor() {
+var p1 = document.getElementById("colorToggle")
+{p1.style.backgroundColor = "skyblue";}
+}
+
+function changeColor2() {
+var pars = document.getElementsByTagName('p')
+    for (var i = 0, length = pars.length; i < length; i++) {
+        pars[i].style.backgroundColor = "pink";
+    }   
+}
+
+function newFunction() {
+alert ('YO! This is function is firing!!');
+var li = document.getElementsByTagName('li')    
+   for (var i = 0, length = li.length; i < length; i++) {
+        li[i].style.backgroundColor = "green";
+    }  
+}
